52930c50d3db61635ea52b67aea1934229218b99
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-07-09 11:03:19 +0800

    Mobile

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,13 @@ import Map from '../components/map'
 import { useState } from 'react'
 const title = require('title')
 import Div100vh from 'react-div-100vh'
-import {useSwipeable} from 'react-swipeable'
+import { useSwipeable } from 'react-swipeable'
 
 export default function Home() {
   const [selectedItem, setSelectedItem] = useState(null)
   const [enlargedBox, setEnlargedBox] = useState(false)
   function handleSelection(value) {
-    console.log(value)
-    console.log(selectedItem)
+    setEnlargedBox(true)
     if (selectedItem !== value) {
       setSelectedItem(value)
     } else {
@@ -18,29 +17,41 @@ export default function Home() {
     }
   }
   const handlers = useSwipeable({
-    onSwipedUp: (eventData) => setEnlargedBox(true),
-    onSwipedDown: (eventData) => setEnlargedBox(false),
-  });
+    onSwipedUp: eventData => setEnlargedBox(true),
+    onSwipedDown: eventData => setEnlargedBox(false),
+  })
   return (
     <Div100vh>
       <Box
         as="main"
         sx={{ bg: '#E6E4E0', maxHeight: '100vh', overflow: 'hidden' }}
       >
-  
         <Box
           sx={{
             position: 'fixed',
             zIndex: '999',
-            height: [enlargedBox ? '60vh': '20vh', '100vh'],
+            height: [enlargedBox ? '60vh' : '22vh', '100vh'],
+            transition: 'height ease-in 0.5s',
             width: ['100%', '400px'],
             p: [0, 3],
             bottom: 0,
           }}
+          onClick={() => setEnlargedBox(!enlargedBox)}
           {...handlers}
         >
           <Box sx={{ bg: '#E6E4E0', boxShadow: 'card', height: '100%', p: 3 }}>
             <Box sx={{ height: 'calc(100% - 36px)', overflowY: 'scroll' }}>
+              <Box
+                sx={{
+                  height: [enlargedBox ? '0px' : '5px', '0px'],
+                  borderRadius: 2,
+                  bg: '#c7c2bb',
+                  width: '60px',
+                  mb: [enlargedBox ? 0 : 2, 0],
+                  opacity: [enlargedBox ? 0 : 1, 0],
+                  transition: 'ease-in 0.5s',
+                }}
+              ></Box>
               <Heading as="h1">
                 {!selectedItem ? 'Comiteer' : selectedItem.name}
               </Heading>
@@ -71,6 +82,11 @@ export default function Home() {
         </Box>
         <Map setSelectedItem={handleSelection} selectedItem={selectedItem} />
       </Box>
+      <style>{`
+      html {
+        overscroll-behavior-y: none!important;
+      }
+      `}</style>
     </Div100vh>
   )
 }
