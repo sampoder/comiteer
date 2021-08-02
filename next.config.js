0c74ef8b52120e28b6e14c9c18521505038f0e2f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-02 15:42:14 +0800

    PWA!

--- next.config.js
+++ next.config.js
@@ -1,29 +1,35 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
-module.exports = withMDX({
-  pageExtensions: ['js', 'mdx'],
-  images: {
-    domains: [
-      'cloud-okol6b1vm-hack-club-bot.vercel.app',
-      'cloud-qmweg7d8y-hack-club-bot.vercel.app',
-      'volunteer.gov.sg',
-      'lp-cms-production.imgix.net',
-      'www.volunteer.gov.sg',
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('_http_common')
-      // config.externals.push('encoding');
-    }
-    return config
-  },
-  async redirects() {
-    return [
-      {
-        source: '/callback',
-        destination: '/api/singpass',
-        permanent: true,
-      },
-    ]
-  },
-})
+const withPWA = require('next-pwa')
+module.exports = withPWA(
+  withMDX({
+    pwa: {
+      dest: 'public',
+    },
+    pageExtensions: ['js', 'mdx'],
+    images: {
+      domains: [
+        'cloud-okol6b1vm-hack-club-bot.vercel.app',
+        'cloud-qmweg7d8y-hack-club-bot.vercel.app',
+        'volunteer.gov.sg',
+        'lp-cms-production.imgix.net',
+        'www.volunteer.gov.sg',
+      ],
+    },
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        config.externals.push('_http_common')
+        // config.externals.push('encoding');
+      }
+      return config
+    },
+    async redirects() {
+      return [
+        {
+          source: '/callback',
+          destination: '/api/singpass',
+          permanent: true,
+        },
+      ]
+    },
+  }),
+)
