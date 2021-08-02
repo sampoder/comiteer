c6ae6d8c1faa7574d313d92e1ae7408ac18ee8c5
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-02 15:48:45 +0800

    PWA 2

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import Meta from '../components/meta'
 import theme from '../lib/theme'
 import { ThemeProvider } from 'theme-ui'
 import { withModalTheme } from '@mattjennings/react-modal'
@@ -33,6 +32,81 @@ const App = ({ Component, pageProps }) => {
   }
   return (
     <ThemeProvider theme={withModalTheme(theme)}>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#2196f3" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#2196f3" />
+      </Head>
       <Login />
       <div style={{ position: 'relative' }}>
         <div style={{ zIndex: '100', position: 'relative' }}>
